9c30696b1503aadab384d484d1c01d24793ce46c
Karosene <adrianberta08@gmail.com>
2024-10-01 13:19:37 +0200

    first commit

---
+++ twitterphp/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/src/output.css">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <script src="script.js"></script>
+    <!-- Use Font Awesome 5 CDN link -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+</head>
+
+<body class="w-full h-10 bg-black">
+    
+</body>
+</html>
+
+<?php
+
+?>
